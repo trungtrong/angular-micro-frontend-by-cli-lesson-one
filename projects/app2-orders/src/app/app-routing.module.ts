dcf99d9f55d976746d81aeecab4fe05395ebe73b
Trong Ngo <trong.ngotrung@gmail.com>
2022-03-10 22:41:14 +0700

    [Step 7 - 5] Update the Routes to use the federated module and components

--- projects/app2-orders/src/app/app-routing.module.ts
+++ projects/app2-orders/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//
+import { FileType, MfeUtil } from "@utils";
 
 const routes: Routes = [
+    {
+        path: '',
+        component: await new MfeUtil().loadRemoteFile({
+            remoteName: "home",
+            remoteEntry: `http://localhost:4203/remoteHome.js`,
+            exposedFile: "HomeComponent",
+            exposeFileType: FileType.Component,
+        }).then((m) => m.HomeComponent),
+    },
+    {
+        path: 'restaurants',
+        loadChildren: () => new MfeUtil().loadRemoteFile({
+            remoteName: "restaurant",
+            remoteEntry: `http://localhost:4204/remoteRestaurant.js`,
+            exposedFile: "RestaurantModule",
+            exposeFileType: FileType.Module
+        }).then((m) => m.RestaurantModule),
+    },
     {
         path: 'order',
         loadChildren: () => import('./order/order.module').then(m => m.OrderModule),
